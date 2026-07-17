0fe345905840decf25a550042efc1d0fe4515e0a
MiniMines Associate <itassociate@m-mines.com>
2026-07-17 15:21:08 +0530

    fix(bd): adjust tab positions to perfectly interleave with companies and people at position 8

--- src/navigation-menu-items/export-shipment.ts
+++ src/navigation-menu-items/export-shipment.ts
@@ -4,7 +4,7 @@ export default defineNavigationMenuItem({
   universalIdentifier: '64d4976d-a523-4b3c-be48-3bc87a0bcd0e',
   name: 'exportShipment',
   icon: 'IconList',
-  position: -1,
+  position: 15,
   type: NavigationMenuItemType.OBJECT,
   targetObjectUniversalIdentifier: '04acd819-f079-4dde-b36d-1eb14b47167d',
 });
