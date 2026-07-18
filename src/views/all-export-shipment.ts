b673191d5f4c1b14c26a5bc320d20e565a0d436e
MiniMines Associate <itassociate@m-mines.com>
2026-07-18 15:35:45 +0530

    fix: revert explicit view fields to resolve metadata sync crash

--- src/views/all-export-shipment.ts
+++ src/views/all-export-shipment.ts
@@ -5,56 +5,7 @@ export default defineView({
   name: 'all-export-shipment',
   objectUniversalIdentifier: '04acd819-f079-4dde-b36d-1eb14b47167d',
   icon: 'IconList',
-  fields: [
-    {
-      universalIdentifier: 'e72a8182-82f9-4433-a487-cfde7706f687',
-      fieldMetadataUniversalIdentifier: 'c83b1a20-3b99-4c22-b7e2-45e0f33381aa',
-      isVisible: true,
-      position: 0,
-    },
-    {
-      universalIdentifier: 'c42a8182-82f9-4433-a487-cfde7706f687',
-      fieldMetadataUniversalIdentifier: 'd45b1a20-3b99-4c22-b7e2-45e0f33381bb',
-      isVisible: true,
-      position: 1,
-    },
-    {
-      universalIdentifier: 'b52a8182-82f9-4433-a487-cfde7706f687',
-      fieldMetadataUniversalIdentifier: 'e56b1a20-3b99-4c22-b7e2-45e0f33381cc',
-      isVisible: true,
-      position: 2,
-    },
-    {
-      universalIdentifier: 'a62a8182-82f9-4433-a487-cfde7706f687',
-      fieldMetadataUniversalIdentifier: 'f67b1a20-3b99-4c22-b7e2-45e0f33381dd',
-      isVisible: true,
-      position: 3,
-    },
-    {
-      universalIdentifier: '972a8182-82f9-4433-a487-cfde7706f687',
-      fieldMetadataUniversalIdentifier: 'a78b1a20-3b99-4c22-b7e2-45e0f33381ee',
-      isVisible: true,
-      position: 4,
-    },
-    {
-      universalIdentifier: '882a8182-82f9-4433-a487-cfde7706f687',
-      fieldMetadataUniversalIdentifier: 'b89b1a20-3b99-4c22-b7e2-45e0f33381ff',
-      isVisible: true,
-      position: 5,
-    },
-    {
-      universalIdentifier: '792a8182-82f9-4433-a487-cfde7706f687',
-      fieldMetadataUniversalIdentifier: 'c90b1a20-3b99-4c22-b7e2-45e0f3338100',
-      isVisible: true,
-      position: 6,
-    },
-    {
-      universalIdentifier: '682a8182-82f9-4433-a487-cfde7706f687',
-      fieldMetadataUniversalIdentifier: 'e86b1a20-3b99-4c22-b7e2-45e0f33382aa',
-      isVisible: true,
-      position: 7,
-    }
-  ],
+  fields: [],
   // filters: [
   //   {
   //     universalIdentifier: '...',
--- src/views/export-shipment-record-page-fields.ts
+++ src/views/export-shipment-record-page-fields.ts
@@ -9,56 +9,7 @@ export default defineView({
   objectUniversalIdentifier: '04acd819-f079-4dde-b36d-1eb14b47167d',
   type: 'FIELDS_WIDGET' as any,
   icon: 'IconList',
-  fields: [
-    {
-      universalIdentifier: 'f72a8182-82f9-4433-a487-cfde7706f687',
-      fieldMetadataUniversalIdentifier: 'c83b1a20-3b99-4c22-b7e2-45e0f33381aa',
-      isVisible: true,
-      position: 0,
-    },
-    {
-      universalIdentifier: 'd42a8182-82f9-4433-a487-cfde7706f687',
-      fieldMetadataUniversalIdentifier: 'd45b1a20-3b99-4c22-b7e2-45e0f33381bb',
-      isVisible: true,
-      position: 1,
-    },
-    {
-      universalIdentifier: 'e52a8182-82f9-4433-a487-cfde7706f687',
-      fieldMetadataUniversalIdentifier: 'e56b1a20-3b99-4c22-b7e2-45e0f33381cc',
-      isVisible: true,
-      position: 2,
-    },
-    {
-      universalIdentifier: 'f62a8182-82f9-4433-a487-cfde7706f687',
-      fieldMetadataUniversalIdentifier: 'f67b1a20-3b99-4c22-b7e2-45e0f33381dd',
-      isVisible: true,
-      position: 3,
-    },
-    {
-      universalIdentifier: 'a72a8182-82f9-4433-a487-cfde7706f687',
-      fieldMetadataUniversalIdentifier: 'a78b1a20-3b99-4c22-b7e2-45e0f33381ee',
-      isVisible: true,
-      position: 4,
-    },
-    {
-      universalIdentifier: 'b82a8182-82f9-4433-a487-cfde7706f687',
-      fieldMetadataUniversalIdentifier: 'b89b1a20-3b99-4c22-b7e2-45e0f33381ff',
-      isVisible: true,
-      position: 5,
-    },
-    {
-      universalIdentifier: 'c92a8182-82f9-4433-a487-cfde7706f687',
-      fieldMetadataUniversalIdentifier: 'c90b1a20-3b99-4c22-b7e2-45e0f3338100',
-      isVisible: true,
-      position: 6,
-    },
-    {
-      universalIdentifier: 'e82a8182-82f9-4433-a487-cfde7706f687',
-      fieldMetadataUniversalIdentifier: 'e86b1a20-3b99-4c22-b7e2-45e0f33382aa',
-      isVisible: true,
-      position: 7,
-    }
-  ],
+  fields: [],
   // filters: [
   //   {
   //     universalIdentifier: '...',
