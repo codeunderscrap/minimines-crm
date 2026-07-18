c941468e33c0ff8f5da75a88b745e69d5e0f91fe
MiniMines Associate <itassociate@m-mines.com>
2026-07-18 15:08:28 +0530

    fix: explicitly map export shipment fields to views to force display

--- src/views/all-export-shipment.ts
+++ src/views/all-export-shipment.ts
@@ -5,8 +5,56 @@ export default defineView({
   name: 'all-export-shipment',
   objectUniversalIdentifier: '04acd819-f079-4dde-b36d-1eb14b47167d',
   icon: 'IconList',
-  position: 0,
-  fields: [],
+  fields: [
+    {
+      universalIdentifier: generateDefaultFieldUniversalIdentifier('b8c4a1ab-f0e2-47d2-88cb-3cf8cc90d407', 'c83b1a20-3b99-4c22-b7e2-45e0f33381aa'),
+      fieldMetadataUniversalIdentifier: 'c83b1a20-3b99-4c22-b7e2-45e0f33381aa',
+      isVisible: true,
+      position: 0,
+    },
+    {
+      universalIdentifier: generateDefaultFieldUniversalIdentifier('b8c4a1ab-f0e2-47d2-88cb-3cf8cc90d407', 'd45b1a20-3b99-4c22-b7e2-45e0f33381bb'),
+      fieldMetadataUniversalIdentifier: 'd45b1a20-3b99-4c22-b7e2-45e0f33381bb',
+      isVisible: true,
+      position: 1,
+    },
+    {
+      universalIdentifier: generateDefaultFieldUniversalIdentifier('b8c4a1ab-f0e2-47d2-88cb-3cf8cc90d407', 'e56b1a20-3b99-4c22-b7e2-45e0f33381cc'),
+      fieldMetadataUniversalIdentifier: 'e56b1a20-3b99-4c22-b7e2-45e0f33381cc',
+      isVisible: true,
+      position: 2,
+    },
+    {
+      universalIdentifier: generateDefaultFieldUniversalIdentifier('b8c4a1ab-f0e2-47d2-88cb-3cf8cc90d407', 'f67b1a20-3b99-4c22-b7e2-45e0f33381dd'),
+      fieldMetadataUniversalIdentifier: 'f67b1a20-3b99-4c22-b7e2-45e0f33381dd',
+      isVisible: true,
+      position: 3,
+    },
+    {
+      universalIdentifier: generateDefaultFieldUniversalIdentifier('b8c4a1ab-f0e2-47d2-88cb-3cf8cc90d407', 'a78b1a20-3b99-4c22-b7e2-45e0f33381ee'),
+      fieldMetadataUniversalIdentifier: 'a78b1a20-3b99-4c22-b7e2-45e0f33381ee',
+      isVisible: true,
+      position: 4,
+    },
+    {
+      universalIdentifier: generateDefaultFieldUniversalIdentifier('b8c4a1ab-f0e2-47d2-88cb-3cf8cc90d407', 'b89b1a20-3b99-4c22-b7e2-45e0f33381ff'),
+      fieldMetadataUniversalIdentifier: 'b89b1a20-3b99-4c22-b7e2-45e0f33381ff',
+      isVisible: true,
+      position: 5,
+    },
+    {
+      universalIdentifier: generateDefaultFieldUniversalIdentifier('b8c4a1ab-f0e2-47d2-88cb-3cf8cc90d407', 'c90b1a20-3b99-4c22-b7e2-45e0f3338100'),
+      fieldMetadataUniversalIdentifier: 'c90b1a20-3b99-4c22-b7e2-45e0f3338100',
+      isVisible: true,
+      position: 6,
+    },
+    {
+      universalIdentifier: generateDefaultFieldUniversalIdentifier('b8c4a1ab-f0e2-47d2-88cb-3cf8cc90d407', 'e86b1a20-3b99-4c22-b7e2-45e0f33382aa'),
+      fieldMetadataUniversalIdentifier: 'e86b1a20-3b99-4c22-b7e2-45e0f33382aa',
+      isVisible: true,
+      position: 7,
+    }
+  ],
   // filters: [
   //   {
   //     universalIdentifier: '...',
--- src/views/export-shipment-record-page-fields.ts
+++ src/views/export-shipment-record-page-fields.ts
@@ -9,8 +9,56 @@ export default defineView({
   objectUniversalIdentifier: '04acd819-f079-4dde-b36d-1eb14b47167d',
   type: 'FIELDS_WIDGET' as any,
   icon: 'IconList',
-  position: 0,
-  fields: [],
+  fields: [
+    {
+      universalIdentifier: generateDefaultFieldUniversalIdentifier('e8ff2a81-82f9-4433-a487-cfde7706f687', 'c83b1a20-3b99-4c22-b7e2-45e0f33381aa'),
+      fieldMetadataUniversalIdentifier: 'c83b1a20-3b99-4c22-b7e2-45e0f33381aa',
+      isVisible: true,
+      position: 0,
+    },
+    {
+      universalIdentifier: generateDefaultFieldUniversalIdentifier('e8ff2a81-82f9-4433-a487-cfde7706f687', 'd45b1a20-3b99-4c22-b7e2-45e0f33381bb'),
+      fieldMetadataUniversalIdentifier: 'd45b1a20-3b99-4c22-b7e2-45e0f33381bb',
+      isVisible: true,
+      position: 1,
+    },
+    {
+      universalIdentifier: generateDefaultFieldUniversalIdentifier('e8ff2a81-82f9-4433-a487-cfde7706f687', 'e56b1a20-3b99-4c22-b7e2-45e0f33381cc'),
+      fieldMetadataUniversalIdentifier: 'e56b1a20-3b99-4c22-b7e2-45e0f33381cc',
+      isVisible: true,
+      position: 2,
+    },
+    {
+      universalIdentifier: generateDefaultFieldUniversalIdentifier('e8ff2a81-82f9-4433-a487-cfde7706f687', 'f67b1a20-3b99-4c22-b7e2-45e0f33381dd'),
+      fieldMetadataUniversalIdentifier: 'f67b1a20-3b99-4c22-b7e2-45e0f33381dd',
+      isVisible: true,
+      position: 3,
+    },
+    {
+      universalIdentifier: generateDefaultFieldUniversalIdentifier('e8ff2a81-82f9-4433-a487-cfde7706f687', 'a78b1a20-3b99-4c22-b7e2-45e0f33381ee'),
+      fieldMetadataUniversalIdentifier: 'a78b1a20-3b99-4c22-b7e2-45e0f33381ee',
+      isVisible: true,
+      position: 4,
+    },
+    {
+      universalIdentifier: generateDefaultFieldUniversalIdentifier('e8ff2a81-82f9-4433-a487-cfde7706f687', 'b89b1a20-3b99-4c22-b7e2-45e0f33381ff'),
+      fieldMetadataUniversalIdentifier: 'b89b1a20-3b99-4c22-b7e2-45e0f33381ff',
+      isVisible: true,
+      position: 5,
+    },
+    {
+      universalIdentifier: generateDefaultFieldUniversalIdentifier('e8ff2a81-82f9-4433-a487-cfde7706f687', 'c90b1a20-3b99-4c22-b7e2-45e0f3338100'),
+      fieldMetadataUniversalIdentifier: 'c90b1a20-3b99-4c22-b7e2-45e0f3338100',
+      isVisible: true,
+      position: 6,
+    },
+    {
+      universalIdentifier: generateDefaultFieldUniversalIdentifier('e8ff2a81-82f9-4433-a487-cfde7706f687', 'e86b1a20-3b99-4c22-b7e2-45e0f33382aa'),
+      fieldMetadataUniversalIdentifier: 'e86b1a20-3b99-4c22-b7e2-45e0f33382aa',
+      isVisible: true,
+      position: 7,
+    }
+  ],
   // filters: [
   //   {
   //     universalIdentifier: '...',
