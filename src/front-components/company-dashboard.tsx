60ef7a2021bdc42d7844b7d7bf3ab48dd1f6b6cb
MiniMines Associate <itassociate@m-mines.com>
2026-08-08 15:22:29 +0530

    fix(ui): resolve jsx syntax error from regex replacement

--- src/front-components/company-dashboard.tsx
+++ src/front-components/company-dashboard.tsx
@@ -275,7 +275,7 @@ const CompanyDashboard = () => {
             <div style={{ color: BRAND.text, fontSize: '12px', marginTop: '4px' }}>Select a client to manage their interactions</div>
             
             <a href="/page/a1cd845e-046a-4147-a04f-895290bf6b73"
-            className="add-btn" style={{ marginTop: \'16px\', justifyContent: \'center\' }}>
+            className="add-btn" style={{ marginTop: '16px', justifyContent: 'center' }}>
               + Add New Company
             </a>
           </div>
