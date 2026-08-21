c70512393b87b8c0772463f1831be48594bb2df9
MiniMines Associate <itassociate@m-mines.com>
2026-08-21 13:14:49 +0530

    chore: change quotation approval status from CEO Approved to HOD Approved

--- src/objects/quotation.ts
+++ src/objects/quotation.ts
@@ -41,7 +41,7 @@ export default defineObject({
       options: [
         { position: 0, color: 'gray', label: 'Draft', value: 'DRAFT' },
         { position: 1, color: 'orange', label: 'HOD Review', value: 'HOD_REVIEW' },
-        { position: 2, color: 'green', label: 'CEO Approved', value: 'CEO_APPROVED' },
+        { position: 2, color: 'green', label: 'HOD Approved', value: 'HOD_APPROVED' },
         { position: 3, color: 'blue', label: 'Converted to Order', value: 'CONVERTED_TO_ORDER' },
         { position: 4, color: 'red', label: 'Rejected', value: 'REJECTED' },
       ]
